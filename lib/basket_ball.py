be5ae13d5a25fb05883205982654e94fa2a982ae
Cherise Foster <cherise.foster@outlook.com>
2023-07-02 17:33:37 -0600

    error with importing test module

--- lib/basket_ball.py
+++ lib/basket_ball.py
@@ -182,3 +182,14 @@ def game_dict():
             ]
         }
     }
+
+
+def num_points_per_game(player_name):
+    game_data = game_dict()
+
+    for team in game_data.values():
+        for player in team['players']:
+            if player['name'] == player_name:
+                return player['points_per_game']
+            
+    return None
